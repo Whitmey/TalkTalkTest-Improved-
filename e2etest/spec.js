14ed0d82111191b7d8f8aadd8b5ac25d0057aad5
Will Whitmey <w.whitmey@outlook.com>
2017-03-05 14:37:06 +0000

    testing the tests

--- e2etest/spec.js
+++ e2etest/spec.js
@@ -1,33 +1,28 @@
 // spec.js
-describe('Protractor Demo App', function() {
+describe('TalkTalkApp', function() {
   // var firstNumber = element(by.model('first'));
   // var secondNumber = element(by.model('second'));
   // var goButton = element(by.id('gobutton'));
   // var latestResult = element(by.binding('latest'));
 
   beforeEach(function() {
-    browser.get('file:///Users/WillWhitmey/TalkTalkTest-Improved-/index.html');
+    browser.get('http://www.willwhitmey.io/TalkTalkTest-Improved-/');
   });
 
   it('should have a title', function() {
     expect(browser.getTitle()).toEqual('TalkTalk');
   });
 
-  // it('should add one and two', function() {
-  //   firstNumber.sendKeys(1);
-  //   secondNumber.sendKeys(2);
-  //
-  //   goButton.click();
-  //
-  //   expect(latestResult.getText()).toEqual('3');
-  // });
-  //
-  // it('should add four and six', function() {
-  //   firstNumber.sendKeys(4);
-  //   secondNumber.sendKeys(6);
-  //
-  //   goButton.click();
-  //
-  //   expect(latestResult.getText()).toEqual('10');
-  // });
+  it('should have a list of contacts', function() {
+    expect(element.all(by.repeater('main in main.all')).count()).toEqual(5);
+  });
+
+  it('should add a contact', function() {
+    element(by.model('main.addUser.name')).sendKeys('Will Whitmey');
+    element(by.model('main.addUser.email')).sendKeys('w.whitmey@outlook.com');
+    element(by.model('main.addUser.job')).sendKeys('Web Developer');
+    element(by.model('main.addUser.location')).sendKeys('London');
+    element(by.model('main.addUser.tag')).sendKeys('Friend');
+    element(by.id)
+  })
 });
